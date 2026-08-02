de5639b19d13ec45d93b6d37674cb473f621c46e
Handoko <hd.doko22@gmail.com>
2026-08-02 09:03:47 +0700

    style(layout): remove navbar home link

--- resources/js/layouts/AppLayout.vue
+++ resources/js/layouts/AppLayout.vue
@@ -6,9 +6,6 @@
         <li class="nav-item">
           <a class="nav-link" data-widget="pushmenu" href="#" role="button"><i class="fas fa-bars"></i></a>
         </li>
-        <li class="nav-item d-none d-sm-inline-block">
-          <Link href="/dashboard" class="nav-link"><i class="fas fa-home mr-1"></i> Beranda</Link>
-        </li>
       </ul>
 
       <!-- Right navbar links -->
